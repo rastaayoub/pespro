7e99cabe2d713d9430815159ea2ae94109823565
rastaayoub <51851888+rastaayoub@users.noreply.github.com>
2019-12-14 18:40:04 +0100

    pespro

--- system/database.php
+++ system/database.php
@@ -1,13 +1,13 @@
 <?php
 /* IF YOU ADD DATABASE SETTINGS MANUALLY, DELETE CODE FROM BELOW */
-if(!defined('INSTALL')){
-	header("Location: install");
-}
+#if(!defined('INSTALL')){
+#	header("Location: install");
+#}
 /* -------------------------------------------------------------- */
 
-$config['sql_host']       = 'localhost';
-$config['sql_username']   = 'user';			// Database Username
-$config['sql_password']   = '';				// Database Password
-$config['sql_database']   = '';				// The database
+$config['sql_host']       = 'remotemysql.com';
+$config['sql_username']   = '96prXqb6ER';			// Database Username
+$config['sql_password']   = 'rgW1aYcnZO';				// Database Password
+$config['sql_database']   = '96prXqb6ER';				// The database
 $config['sql_extenstion'] = 'MySQL';		// MySQL or MySQLi
 ?>
